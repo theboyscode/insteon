3a6923770d3bf47e551d53a8420ba1ee029ae2d6
esorense <soren_the_boy@yahoo.com>
2014-03-02 20:11:04 -0800

    added log file dire

--- insteon_scheduler_v01d01_28FEB2014.py
+++ insteon_scheduler_v01d01_28FEB2014.py
@@ -15,7 +15,7 @@ FILENAME = "/home/pi/Insteon/data.csv"
 
 def log_str(str_to_log):
     #open log file
-    filename = strftime("/home/pi/Insteon/%d_%b_%Y.log", localtime())
+    filename = strftime("/home/pi/Insteon/log/%d_%b_%Y.log", localtime())
     LOG_FILE = open(filename,'a')
     LOG_FILE.write(str(str_to_log)+"\n")
     #print str(str_to_log)
